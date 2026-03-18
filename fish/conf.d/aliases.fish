82cfe21b81d46c6ab72fb23e250efdd045ced67f
David Nikolov <Lummotm@proton.me>
2026-03-18 17:47:03 +0100

    new vers

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,5 +1,7 @@
 alias sd "$HOME/termux-dotfiles/setup.sh --sync" # sync dotfiles
 alias snf "$HOME/scripts/sync-obsidian-git.sh -f" #sync notes forced
+alias sne "$HOME/scripts/sync-obsidian-git.sh -fe" # forced external
 alias sn "$HOME/scripts/sync-obsidian-git.sh" #sync notes 
 alias startssh "$HOME/scripts/startssh.sh"
 alias clc "clear"
+alias gs "cd $HOME/obsidian && git status && cd -"
