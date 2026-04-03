6e3013e1d82b133446b28703df0f886685dcbfd2
David Nikolov <Lummotm@proton.me>
2026-04-03 16:10:49 +0200

    ahora si

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -5,4 +5,5 @@ alias sa "$HOME/scripts/sync-all.sh" #sync notes
 alias startssh "$HOME/scripts/startssh.sh"
 alias clc "clear"
 alias gs "cd $HOME/obsidian && git status && cd -"
+alias cerrar "pkill -f ssh; exit"
 alias help-config "cat ~/termux-dotfiles/help_config.md"
